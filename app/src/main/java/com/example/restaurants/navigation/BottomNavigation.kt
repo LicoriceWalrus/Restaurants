ba0756506b11fa49fc3f1659eeb419bacc7d8483
Bocharov Roman <bocharovRO@pay-trans.ru>
2022-06-15 18:21:46 +0300

    added icons to bottom bar menu

--- app/src/main/java/com/example/restaurants/navigation/BottomNavigation.kt
+++ app/src/main/java/com/example/restaurants/navigation/BottomNavigation.kt
@@ -1,13 +1,23 @@
 package com.example.restaurants.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.restaurants.R
 
 @Composable
 fun BottomNavigation(
@@ -23,9 +33,14 @@ fun BottomNavigation(
         val currentRoute = navBackStackEntry?.destination?.route
         items.forEach { item ->
             BottomNavigationItem(
+                modifier = Modifier.background(colorResource(id = R.color.bottom_bar_color)),
                 icon = {
-                    //=TODO Добавить иконки
-//                    Icon(painterResource(id = item.icon), contentDescription = item.title)
+                    Icon(
+                        modifier = Modifier.size(30.dp),
+                        painter = painterResource(id = item.icon),
+                        contentDescription = item.title,
+                        tint = Color.Unspecified
+                    )
                 },
                 label = {
                     Text(
@@ -51,4 +66,10 @@ fun BottomNavigation(
             )
         }
     }
+}
+
+@Composable
+@Preview
+private fun BottomNavigationPreview() {
+    BottomNavigation(rememberNavController())
 }
--- app/src/main/java/com/example/restaurants/navigation/Screens.kt
+++ app/src/main/java/com/example/restaurants/navigation/Screens.kt
@@ -1,7 +1,11 @@
 package com.example.restaurants.navigation
 
-sealed class Screens(var title: String, val route: String) {
-    object Restaurants : Screens(title = "Рестораны", route = "restaurants_screen")
-    object Hits : Screens(title = "Хиты", route = "hits_screen")
-    object Review : Screens(title = "Отзывы", route = "review_screen")
+import com.example.restaurants.R
+
+sealed class Screens(var title: String, val route: String, val icon: Int) {
+    object Restaurants :
+        Screens(title = "Рестораны", route = "restaurants_screen", R.drawable.ic_restaurants_menu)
+
+    object Hits : Screens(title = "Хиты", route = "hits_screen", R.drawable.ic_hits_menu)
+    object Review : Screens(title = "Отзывы", route = "review_screen", R.drawable.ic_reviews_menu)
 }
